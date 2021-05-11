a8e5b25ab342b66f2347cd509826f03b7a4a8346
henryc99 <83266576+henryc99@users.noreply.github.com>
2021-05-11 15:30:53 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://henryc99.github.io/AI-frog10932602/model.json';
 
 
 function preload() {
